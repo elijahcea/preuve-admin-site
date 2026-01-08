962599551bc98bf0b7495a7371e8f5def5d3e403
elijahcea <elijah.cea01@gmail.com>
2026-01-07 22:05:58 -0500

    init nav routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from "@/routes/Home.vue"
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/routes/HomeView.vue";
+import ProductsView from "@/routes/ProductsView.vue";
+import CollectionsView from "@/routes/CollectionsView.vue";
+import OptionsView from "@/routes/OptionsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/", component: Home },
-    { path: "/products", component: Home },
-    { path: "/collections", component: Home },
-    { path: "/options", component: Home }
+    { path: "/", component: HomeView },
+    { path: "/products", component: ProductsView },
+    { path: "/collections", component: CollectionsView },
+    { path: "/options", component: OptionsView }
   ],
 })
 
