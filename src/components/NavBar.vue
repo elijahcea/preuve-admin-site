2a4893d5c1d2931adf9cc67887b9f8a0ad22d2e4
elijahcea <elijah.cea01@gmail.com>
2026-01-11 23:30:39 -0500

    reduce border-radius in navBar elements

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,11 +2,11 @@
 import { ShoppingBagIcon, TagIcon, SwatchIcon, HomeIcon } from "@heroicons/vue/24/outline";
 
 const iconSize = "size-5";
-const itemStyles = "flex items-center px-2 py-1 gap-2 hover:bg-current/20 rounded-xl"
+const itemStyles = "flex items-center px-2 py-1 gap-2 hover:bg-current/20 rounded"
 </script>
 
 <template>
-    <nav class="p-3 bg-gray-200 inset-shadow-sm font-semibold">
+    <nav class="p-3 bg-gray-300 inset-shadow-sm font-semibold">
         <ul>
             <RouterLink to="/" :class="itemStyles">
                 <HomeIcon :class="iconSize"></HomeIcon>
