3ae46d615b7bf7aa8521ced7829d1fa197806b29
elijahcea <elijah.cea01@gmail.com>
2026-01-20 19:54:27 -0500

    add new route for products and collections

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,34 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "@/routes/HomeView.vue";
-import ProductsView from "@/routes/ProductsView.vue";
-import CollectionsView from "@/routes/CollectionsView.vue";
-import OptionsView from "@/routes/OptionsView.vue";
-import ProductDetailsView from "@/routes/ProductDetailsView.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/routes/HomeView.vue'
+import ProductsView from '@/routes/products/ProductsView.vue'
+import CollectionsView from '@/routes/collections/CollectionsView.vue'
+import OptionsView from '@/routes/OptionsView.vue'
+import ProductDetailsView from '@/routes/products/ProductDetailsView.vue'
+import CollectionDetailsView from '@/routes/collections/CollectionDetailsView.vue'
+import NewProductView from '@/routes/products/NewProductView.vue'
+import NewCollectionView from '@/routes/collections/NewCollectionView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/", component: HomeView },
-    { path: "/products",
+    { path: '/', component: HomeView },
+    {
+      path: '/products',
       children: [
-        { path: "", component: ProductsView },
-        { path: ":id", name: "productDetails" , component: ProductDetailsView, props: true }
-      ]
+        { path: '', component: ProductsView },
+        { path: 'new', name: 'newProduct', component: NewProductView },
+        { path: ':id', name: 'productDetails', component: ProductDetailsView, props: true },
+      ],
     },
-    { path: "/collections", component: CollectionsView },
-    { path: "/options", component: OptionsView }
+    {
+      path: '/collections',
+      children: [
+        { path: '', component: CollectionsView },
+        { path: 'new', name: 'newCollection', component: NewCollectionView },
+        { path: ':id', name: 'collectionDetails', component: CollectionDetailsView, props: true },
+      ],
+    },
+    { path: '/options', component: OptionsView },
   ],
 })
 
