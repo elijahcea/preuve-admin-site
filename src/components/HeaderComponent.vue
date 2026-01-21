4b29d8c9155dba483b0085039d42db4c2b12ebf1
elijahcea <elijah.cea01@gmail.com>
2026-01-20 19:51:32 -0500

    add icon to header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-    
+import { BuildingStorefrontIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
-    <header class="bg-foreground text-background">
-        <nav class="flex justify-between items-center px-5 py-3 font-semibold">
-            <RouterLink to="/">Preuve Admin</RouterLink>
-            <button class="bg-current/10 rounded p-2 hover:bg-current/20">Login</button>
-        </nav>
-    </header>
+  <header class="bg-foreground text-background">
+    <nav class="flex justify-between items-center px-5 py-3 font-semibold">
+      <div class="flex gap-2 items-center">
+        <BuildingStorefrontIcon class="size-5" />
+        <RouterLink to="/">Preuve Admin</RouterLink>
+      </div>
+      <button class="bg-current/10 rounded p-2 hover:bg-current/20 text-sm">Login</button>
+    </nav>
+  </header>
 </template>
