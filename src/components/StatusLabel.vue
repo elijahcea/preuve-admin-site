af78a452d102f9944e1f59ce05592fbe9aa06a92
elijahcea <elijah.cea01@gmail.com>
2026-01-27 10:38:52 -0500

    create statusLabel component

---
+++ src/components/StatusLabel.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { status } = defineProps<{
+  status?: boolean
+}>()
+</script>
+
+<template>
+  <div
+    :class="[
+      'rounded-2xl border p-1 text-xs',
+      status
+        ? 'border-blue-300 bg-blue-300 text-blue-900'
+        : 'border-gray-300 bg-gray-300 text-gray-900',
+    ]"
+  >
+    {{ status ? 'Active' : 'Draft' }}
+  </div>
+</template>
