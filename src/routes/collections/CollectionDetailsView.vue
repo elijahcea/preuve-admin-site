a58472859d4f77fa2f5fab93a174f45bfe014527
elijahcea <elijah.cea01@gmail.com>
2026-02-04 14:59:08 -0500

    add queries and dropdowns to collection details

--- src/routes/collections/CollectionDetailsView.vue
+++ src/routes/collections/CollectionDetailsView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import {
   Listbox,
   ListboxButton,
@@ -7,49 +7,89 @@ import {
   ListboxOption,
   ListboxOptions,
 } from '@headlessui/vue'
-import { type Product } from '@/lib/types'
+import { type Collection, type ProductPreview } from '@/lib/types'
 import { ArrowLeftIcon, CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/24/outline'
+import { fetchCollection, fetchProducts } from '@/api/queries'
+import { useQuery } from '@tanstack/vue-query'
+import TitleAndDescription from '@/components/TitleAndDescription.vue'
 
-const props = defineProps({
-  id: String,
+const props = defineProps<{
+  id: string
+}>()
+
+const collectionQuery = useQuery({
+  queryKey: ['collections', () => props.id],
+  queryFn: () => fetchCollection(props.id),
 })
 
-const products: Product[] = []
-const selectedProducts = ref<Product[]>([])
+const productsQuery = useQuery({
+  queryKey: ['products'],
+  queryFn: fetchProducts,
+})
+
+const title = ref<string>('')
+const description = ref<string>('')
+const products = ref<ProductPreview[]>([])
+const selectedProducts = ref<ProductPreview[]>([])
+
+watch(
+  collectionQuery.isSuccess,
+  (isSuccess) => {
+    if (isSuccess && collectionQuery.data.value) {
+      const collection: Collection = JSON.parse(JSON.stringify(collectionQuery.data.value))
+
+      title.value = collection.name
+      description.value = collection.description
+      selectedProducts.value = collection.products
+    }
+  },
+  { immediate: true },
+)
+
+watch(
+  productsQuery.isSuccess,
+  (isSuccess) => {
+    if (isSuccess && productsQuery.data.value) {
+      products.value = JSON.parse(JSON.stringify(productsQuery.data.value))
+    }
+  },
+  { immediate: true },
+)
 </script>
 
 <template>
-  <div class="max-w-2xl flex flex-col m-auto items-center gap-5">
+  <div v-if="collectionQuery.isPending.value">Loading...</div>
+  <div v-else-if="collectionQuery.isError.value">
+    Something went wrong: {{ collectionQuery.error }}
+  </div>
+  <div
+    v-else-if="collectionQuery.data.value"
+    class="max-w-2xl flex flex-col m-auto items-center gap-5"
+  >
     <div class="flex items-center justify-start w-full gap-3">
-      <RouterLink to="/collections" class="p-1 hover:bg-current/20 rounded border border-gray-400">
+      <RouterLink
+        :to="{ name: 'collections' }"
+        class="p-1 hover:bg-current/20 rounded border border-gray-400"
+      >
         <ArrowLeftIcon class="size-5" />
       </RouterLink>
-      <h1 class="font-bold text-xl">{{ props.id }}</h1>
+      <h1 class="font-bold text-xl truncate">{{ title || 'Title' }}</h1>
     </div>
     <form class="w-full text-sm flex flex-col gap-5">
-      <fieldset class="bg-background rounded shadow-lg p-3">
-        <div>
-          <label for="title">Title</label>
-          <input
-            id="title"
-            name="title"
-            type="text"
-            class="border border-gray-300 rounded p-1 w-full my-1"
-          />
+      <!-- Title and Description -->
+      <TitleAndDescription v-model:title="title" v-model:description="description" />
+
+      <section class="bg-background rounded shadow-lg p-3 min-w-0">
+        <div v-if="productsQuery.isPending.value">Loading...</div>
+        <div v-else-if="productsQuery.isError.value">
+          Something went wrong: {{ productsQuery.error.value }}
         </div>
-        <div>
-          <label for="description">Description</label>
-          <textarea
-            id="description"
-            name="description"
-            placeholder="Enter description"
-            rows="5"
-            class="resize-none border border-gray-300 rounded p-1 w-full my-1"
-          ></textarea>
-        </div>
-      </fieldset>
-      <fieldset class="bg-background rounded shadow-lg p-3 min-w-0">
-        <Listbox v-model="selectedProducts" name="selected-products" multiple>
+        <Listbox
+          v-else-if="productsQuery.data.value"
+          v-model="selectedProducts"
+          name="selected-products"
+          multiple
+        >
           <ListboxLabel class="font-semibold">Products</ListboxLabel>
           <ListboxButton
             class="relative border border-gray-300 rounded p-2 w-full flex items-center justify-between my-1"
@@ -83,7 +123,7 @@ const selectedProducts = ref<Product[]>([])
                   <ListboxOption
                     v-for="product in products"
                     :key="product.id"
-                    :value="product"
+                    :value="selectedProducts.find((p) => product.id === p.id) ?? product"
                     v-slot="{ active, selected }"
                     as="template"
                   >
@@ -104,7 +144,7 @@ const selectedProducts = ref<Product[]>([])
             </transition>
           </div>
         </Listbox>
-      </fieldset>
+      </section>
       <div class="m-auto">
         <button type="button" class="bg-gray-300 rounded p-2 hover:bg-gray-300/70">Discard</button>
         <button type="submit" class="bg-blue-300 rounded p-2 ml-2 hover:bg-blue-300/70">
