bdffa80c49b38fd61d2b2529f33736d16d11e46e
elijahcea <elijah.cea01@gmail.com>
2026-02-05 14:39:11 -0500

    create InventoryPanel and PricingPanel components

---
+++ src/components/InventoryPanel.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const sku = defineModel<string>('sku', { required: true })
+const quantity = defineModel<number>('quantity', { required: true })
+</script>
+
+<template>
+  <section class="bg-background rounded shadow-lg p-3">
+    <h2 class="font-semibold mb-4">Inventory</h2>
+    <div class="flex justify-center gap-2">
+      <div class="w-full">
+        <label for="sku">SKU (Stock Keeping Unit)</label>
+        <input
+          id="sku"
+          name="sku"
+          type="text"
+          v-model="sku"
+          class="border border-gray-300 rounded p-2 w-full my-1"
+        />
+      </div>
+      <div class="w-full">
+        <label for="quantity">Quantity</label>
+        <input
+          id="quantity"
+          name="quantity"
+          type="number"
+          required
+          v-model="quantity"
+          class="border border-gray-300 rounded p-2 w-full my-1"
+        />
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/components/PricingPanel.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const { currencySymbol } = defineProps<{
+  currencySymbol: string
+}>()
+
+const price = defineModel<number>({ required: true })
+</script>
+
+<template>
+  <section class="bg-background rounded shadow p-3">
+    <h2 class="font-semibold mb-4">Pricing</h2>
+    <label for="price">Price</label>
+    <div class="flex items-center border border-gray-300 rounded">
+      <span class="ml-2 opacity-60">{{ currencySymbol }}</span>
+      <input
+        id="price"
+        name="price"
+        placeholder="0.00"
+        type="number"
+        min="0"
+        required
+        v-model="price"
+        class="w-full p-2"
+      />
+    </div>
+  </section>
+</template>
