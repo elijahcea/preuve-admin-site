7640f0b47bf0d27710a8dcb9e9296d200f366aa5
elijahcea <elijah.cea01@gmail.com>
2026-03-17 12:52:37 -0400

    update editVariantDialog transitions for fluidity

--- src/components/EditVariantDialog.vue
+++ src/components/EditVariantDialog.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Dialog, DialogPanel, DialogTitle, TransitionRoot } from '@headlessui/vue'
+import { Dialog, DialogPanel, DialogTitle, TransitionRoot, TransitionChild } from '@headlessui/vue'
 import PriceInput from './PriceInput.vue'
 
 const isOpen = defineModel<boolean>('isOpen', { required: true })
@@ -29,68 +29,82 @@ const setIsOpen = (value: boolean) => (isOpen.value = value)
 </script>
 
 <template>
-  <TransitionRoot
-    :show="isOpen"
-    as="template"
-    enter="duration-300 ease-out"
-    enter-from="opacity-0"
-    enter-to="opacity-100"
-    leave="duration-200 ease-in"
-    leave-from="opacity-100"
-    leave-to="opacity-0"
-  >
-    <Dialog @close="setIsOpen" class="fixed inset-0 flex items-center justify-center bg-black/25">
-      <DialogPanel class="w-full max-w-md bg-background p-3 rounded-xl">
-        <DialogTitle class="font-semibold mb-3">Edit variant</DialogTitle>
+  <TransitionRoot :show="isOpen" as="template">
+    <Dialog @close="setIsOpen">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black/25"></div>
+      </TransitionChild>
+      <div class="fixed inset-0 flex min-h-full items-center justify-center">
+        <TransitionChild
+          as="template"
+          enter="duration-300 ease-out"
+          enter-from="opacity-0 scale-95"
+          enter-to="opacity-100 scale-100"
+          leave="duration-200 ease-in"
+          leave-from="opacity-100 scale-100"
+          leave-to="opacity-0 scale-95"
+        >
+          <DialogPanel class="w-full max-w-md bg-background p-3 rounded-xl">
+            <DialogTitle class="font-semibold mb-3">Edit variant</DialogTitle>
 
-        <div class="flex justify-center gap-2">
-          <div class="w-full text-sm">
-            <label for="sku">SKU (Stock Keeping Unit)</label>
-            <input
-              id="sku"
-              name="sku"
-              type="text"
-              v-model="variantInfo.sku"
-              class="border border-gray-300 rounded p-1 w-full my-1"
+            <div class="flex justify-center gap-2">
+              <div class="w-full text-sm">
+                <label for="sku">SKU (Stock Keeping Unit)</label>
+                <input
+                  id="sku"
+                  name="sku"
+                  type="text"
+                  v-model="variantInfo.sku"
+                  class="border border-gray-300 rounded p-1 w-full my-1"
+                />
+              </div>
+              <div class="w-full text-sm">
+                <label for="quantity">Quantity</label>
+                <input
+                  required
+                  v-model="variantInfo.inventoryQuantity"
+                  id="quantity"
+                  name="quantity"
+                  type="number"
+                  min="0"
+                  step="1"
+                  class="border border-gray-300 rounded p-1 w-full my-1"
+                />
+              </div>
+            </div>
+
+            <label class="text-sm" for="price">Price</label>
+            <PriceInput
+              input-id="price"
+              :currency-symbol="currencySymbol"
+              v-model="variantInfo.price"
             />
-          </div>
-          <div class="w-full text-sm">
-            <label for="quantity">Quantity</label>
-            <input
-              required
-              v-model="variantInfo.inventoryQuantity"
-              id="quantity"
-              name="quantity"
-              type="number"
-              min="0"
-              step="1"
-              class="border border-gray-300 rounded p-1 w-full my-1"
-            />
-          </div>
-        </div>
 
-        <label class="text-sm" for="price">Price</label>
-        <PriceInput
-          input-id="price"
-          :currency-symbol="currencySymbol"
-          v-model="variantInfo.price"
-        />
-
-        <div class="flex justify-end gap-1 mt-3">
-          <button
-            class="font-semibold rounded bg-cool-gray py-1 px-2 hover:opacity-80"
-            @click="setIsOpen(false)"
-          >
-            Cancel
-          </button>
-          <button
-            class="font-bold rounded py-1 px-2 ml-2 bg-fill text-background hover:opacity-80"
-            @click="emit('saveVariantEdit', { id: variantId, ...variantInfo })"
-          >
-            Done
-          </button>
-        </div>
-      </DialogPanel>
+            <div class="flex justify-end gap-1 mt-3">
+              <button
+                class="font-semibold rounded bg-cool-gray py-1 px-2 hover:opacity-80"
+                @click="setIsOpen(false)"
+              >
+                Cancel
+              </button>
+              <button
+                class="font-bold rounded py-1 px-2 ml-2 bg-fill text-background hover:opacity-80"
+                @click="emit('saveVariantEdit', { id: variantId, ...variantInfo })"
+              >
+                Done
+              </button>
+            </div>
+          </DialogPanel>
+        </TransitionChild>
+      </div>
     </Dialog>
   </TransitionRoot>
 </template>
