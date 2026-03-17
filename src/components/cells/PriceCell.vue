fe8baed3acb2d380e647612e6fd1037f4bdf13aa
elijahcea <elijah.cea01@gmail.com>
2026-03-17 12:52:17 -0400

    create priceCell component

---
+++ src/components/cells/PriceCell.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const { price, currencySymbol } = defineProps<{ price: number; currencySymbol: string }>()
+</script>
+
+<template>
+  <span class="opacity-60">{{ currencySymbol }}</span>
+  <span class="ml-1">{{ Number(price.toFixed(2)) }}</span>
+</template>
