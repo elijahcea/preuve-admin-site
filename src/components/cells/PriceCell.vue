3f30bf77b873be8aaf804ac64c53f96baeda0198
elijahcea <elijah.cea01@gmail.com>
2026-03-18 15:54:22 -0400

    format price in priceCell

--- src/components/cells/PriceCell.vue
+++ src/components/cells/PriceCell.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
-const { price, currencySymbol } = defineProps<{ price: number; currencySymbol: string }>()
+const { price, locale, currencySymbol } = defineProps<{
+  price: number
+  locale: string
+  currencySymbol: string
+}>()
 </script>
 
 <template>
   <span class="opacity-60">{{ currencySymbol }}</span>
-  <span class="ml-1">{{ Number(price.toFixed(2)) }}</span>
+  <span class="ml-1">{{
+    new Intl.NumberFormat(locale, { minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(
+      price,
+    )
+  }}</span>
 </template>
