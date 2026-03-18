69853292fa20ceac4153a175fe355edd8b3dedd2
elijahcea <elijah.cea01@gmail.com>
2026-03-17 20:02:01 -0400

    add grid row to organize sections and add row for editing variants

--- src/routes/products/NewProductView.vue
+++ src/routes/products/NewProductView.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup lang="tsx">
 import { computed, ref, watch } from 'vue'
 import { currencyInfo } from '@/lib/currencyInfo'
 import {
@@ -14,18 +14,25 @@ import {
   type ProductCreateDTO,
   type ProductVariantCreateForm,
 } from '@/lib/types'
-import { ArrowLeftIcon, CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/24/outline'
+import {
+  ArrowLeftIcon,
+  CheckIcon,
+  ChevronUpDownIcon,
+  PencilSquareIcon,
+} from '@heroicons/vue/24/outline'
 import { fetchCollections } from '@/api/queries'
 import { useMutation, useQuery } from '@tanstack/vue-query'
 import TitleAndDescription from '@/components/TitleAndDescription.vue'
 import StatusLabel from '@/components/StatusLabel.vue'
-import PricingPanel from '@/components/PricingPanel.vue'
 import CreateProductOptionsPanel from '@/components/CreateProductOptionsPanel.vue'
 import TableComponent from '@/components/TableComponent.vue'
 import InventoryPanel from '@/components/InventoryPanel.vue'
 import { postNewProduct } from '@/api/mutations'
 import router from '@/router'
 import { createColumnHelper, useVueTable, getCoreRowModel } from '@tanstack/vue-table'
+import PriceInput from '@/components/PriceInput.vue'
+import EditVariantDialog from '@/components/EditVariantDialog.vue'
+import PriceCell from '@/components/cells/PriceCell.vue'
 
 const title = ref('')
 const description = ref('')
@@ -39,6 +46,8 @@ const defaultVariant = ref({
 })
 const options = ref<OptionCreateForm[]>([])
 const variants = ref<ProductVariantCreateForm[]>([])
+const isEditVariantOpen = ref(false)
+const activeEditVariant = ref<ProductVariantCreateForm>()
 
 const validOptions = computed(() =>
   options.value.filter((option) => option.values.some((value) => value.name.length)),
@@ -76,18 +85,39 @@ const columns = [
   columnHelper.accessor('price', {
     header: 'Price',
     sortingFn: 'alphanumeric',
+    cell: (props) => (
+      <PriceCell price={props.cell.getValue()} currencySymbol={currencyInfo.symbol} />
+    ),
   }),
   columnHelper.accessor('inventoryQuantity', {
     header: 'Inventory',
     sortingFn: 'alphanumeric',
+  }),
+  columnHelper.display({
+    id: 'actions',
+    cell: (props) => (
+      <button
+        aria-label="Edit variant"
+        class="p-1 rounded hover:bg-cool-gray"
+        onClick={() => {
+          activeEditVariant.value = variants.value.find((v) => v.id === props.row.id)
+          isEditVariantOpen.value = true
+        }}
+      >
+        <PencilSquareIcon class="size-4" aria-hidden="true" />
+      </button>
+    ),
   }),
 ]
 
 const variantsTable = useVueTable({
   columns,
   data: variants,
+  getRowId: (row) => row.id,
   getCoreRowModel: getCoreRowModel(),
 })
+
+const saveVariantEdit = () => {}
 
 const handleSubmit = () => {
   if (options.value.length < 1) {
@@ -172,133 +202,35 @@ watch(
 
 <template>
   <!-- Heading -->
-  <div class="max-w-4xl mx-auto mb-5 grid grid-cols-3 gap-5">
-    <div class="flex flex-col items-center gap-5 col-span-2">
-      <div class="flex items-center w-full gap-3">
-        <RouterLink
-          :to="{ name: 'products' }"
-          class="p-1 hover:bg-current/20 rounded border border-gray-400"
-        >
-          <ArrowLeftIcon class="size-5" />
-        </RouterLink>
-        <h1 class="font-bold text-xl truncate">{{ title || 'Title' }}</h1>
-        <StatusLabel :status="productStatus" />
-      </div>
-
-      <div v-if="newProductMutation.isPending.value">Creating new product...</div>
-      <form
-        v-else
-        id="newProductForm"
-        @submit.prevent="handleSubmit"
-        class="w-full text-sm flex flex-col gap-5"
+  <div class="max-w-4xl mx-auto mb-5 grid grid-cols-3 gap-5 grid-container">
+    <div class="flex items-center w-full gap-3">
+      <RouterLink
+        :to="{ name: 'products' }"
+        class="p-1 hover:bg-current/20 rounded border border-gray-400"
       >
-        <!-- Title and Description -->
-        <TitleAndDescription v-model:title="title" v-model:description="description" />
-
-        <PricingPanel v-model="defaultVariant.price" :currency-symbol="symbol" />
-        <InventoryPanel
-          v-model:sku="defaultVariant.sku"
-          v-model:quantity="defaultVariant.quantity"
-        />
-
-        <!-- Product organization/collections -->
-        <section class="bg-light rounded-xl shadow-lg p-3 min-w-0">
-          <h2 class="font-semibold mb-4">Product organization</h2>
-          <div v-if="isPending">Loading...</div>
-          <div v-else-if="isError">An error has occured: {{ error }}</div>
-          <Listbox
-            v-else-if="queryData"
-            v-model="selectedCollections"
-            name="selected-collections"
-            multiple
-          >
-            <ListboxLabel>Collections</ListboxLabel>
-            <ListboxButton
-              class="relative border border-gray-300 rounded p-1 w-full flex items-center justify-between my-1"
-            >
-              <div v-if="selectedCollections.length" class="flex gap-1 flex-wrap">
-                <div
-                  v-for="collection in selectedCollections"
-                  :key="collection.id"
-                  class="rounded-xl border border-blue-900 bg-blue-100 max-w-xs p-0.5"
-                >
-                  <p class="truncate">{{ collection.title }}</p>
-                </div>
-              </div>
-              <p v-else>Select collections</p>
-              <span>
-                <ChevronUpDownIcon class="size-5" aria-hidden="true" />
-              </span>
-            </ListboxButton>
-            <div class="relative mt-1">
-              <transition
-                leave-active-class="transition duration-100 ease-in"
-                leave-from-class="opacity-100"
-                leave-to-class="opacity-0"
-              >
-                <ListboxOptions
-                  class="absolute z-50 w-full bg-light shadow-lg rounded border border-gray-300 divide-y divide-gray-100"
-                >
-                  <template v-if="collections.length">
-                    <ListboxOption
-                      v-for="collection in collections"
-                      :key="collection.id"
-                      :value="collection"
-                      v-slot="{ active, selected }"
-                      as="template"
-                    >
-                      <li
-                        :class="[active ? 'bg-blue-100 text-blue-900' : '', 'p-1 cursor-default']"
-                      >
-                        <span v-show="selected" class="absolute left-0 pl-0.5 text-blue-600">
-                          <CheckIcon class="size-5" aria-hidden="true" />
-                        </span>
-                        <p :class="[selected ? 'font-medium' : 'font-normal', 'pl-5 truncate']">
-                          {{ collection.title }}
-                        </p>
-                      </li>
-                    </ListboxOption>
-                  </template>
-                  <ListboxOption v-else disabled class="p-1 opacity-70 cursor-default">
-                    Create collections
-                  </ListboxOption>
-                </ListboxOptions>
-              </transition>
-            </div>
-          </Listbox>
-        </section>
-
-        <!-- Options section -->
-        <section class="bg-light rounded-xl shadow-lg">
-          <div class="p-3 border-b border-gray-200">
-            <h2 class="font-semibold">Options</h2>
-            <p class="text-xs">Define options for the product, e.g. size, color, etc.</p>
-          </div>
-          <CreateProductOptionsPanel v-model:options="options" v-model:variants="variants" />
-        </section>
-
-        <!-- Variants table -->
-        <section v-if="validOptions.length">
-          <TableComponent :table="variantsTable" :is-loading="false" />
-        </section>
-      </form>
+        <ArrowLeftIcon class="size-5" />
+      </RouterLink>
+      <h1 class="font-bold text-xl truncate">{{ title || 'New product' }}</h1>
+      <StatusLabel :status="productStatus" />
     </div>
-    <div>
-      <!-- Save changes or discard buttons -->
-      <div class="mb-3.5">
-        <RouterLink :to="{ name: 'products' }">
-          <button class="font-semibold rounded bg-cool-gray py-1 px-2 hover:opacity-80">
-            Discard
-          </button>
-        </RouterLink>
-        <button
-          type="submit"
-          form="newProductForm"
-          class="font-bold rounded py-1 px-2 ml-2 bg-fill text-background hover:opacity-80"
-        >
-          Save
+
+    <!-- Save changes or discard buttons -->
+    <div class="col-start-3">
+      <RouterLink :to="{ name: 'products' }">
+        <button class="font-semibold rounded bg-cool-gray py-1 px-2 hover:opacity-80">
+          Discard
         </button>
-      </div>
+      </RouterLink>
+      <button
+        type="submit"
+        form="newProductForm"
+        class="font-bold rounded py-1 px-2 ml-2 bg-fill text-background hover:opacity-80"
+      >
+        Save
+      </button>
+    </div>
+
+    <div class="flex flex-col gap-3 col-start-3">
       <!-- Product status -->
       <section class="bg-light rounded-xl shadow-lg p-3">
         <Listbox v-model="productStatus" name="product-status">
@@ -345,6 +277,123 @@ watch(
           </div>
         </Listbox>
       </section>
+
+      <!-- Product organization/collections -->
+      <section class="bg-light rounded-xl shadow-lg p-3 min-w-0 col-start-3">
+        <h2 class="font-semibold mb-4">Product organization</h2>
+        <div v-if="isPending">Loading...</div>
+        <div v-else-if="isError">An error has occured: {{ error }}</div>
+        <Listbox
+          v-else-if="queryData"
+          v-model="selectedCollections"
+          name="selected-collections"
+          multiple
+        >
+          <ListboxLabel>Collections</ListboxLabel>
+          <ListboxButton
+            class="relative border border-gray-300 rounded p-1 w-full flex items-center justify-between my-1"
+          >
+            <div v-if="selectedCollections.length" class="flex gap-1 flex-wrap">
+              <div
+                v-for="collection in selectedCollections"
+                :key="collection.id"
+                class="rounded-xl border border-blue-900 bg-blue-100 max-w-xs p-0.5"
+              >
+                <p class="truncate">{{ collection.title }}</p>
+              </div>
+            </div>
+            <p v-else>Select collections</p>
+            <span>
+              <ChevronUpDownIcon class="size-5" aria-hidden="true" />
+            </span>
+          </ListboxButton>
+          <div class="relative mt-1">
+            <transition
+              leave-active-class="transition duration-100 ease-in"
+              leave-from-class="opacity-100"
+              leave-to-class="opacity-0"
+            >
+              <ListboxOptions
+                class="absolute z-50 w-full bg-light shadow-lg rounded border border-gray-300 divide-y divide-gray-100"
+              >
+                <template v-if="collections.length">
+                  <ListboxOption
+                    v-for="collection in collections"
+                    :key="collection.id"
+                    :value="collection"
+                    v-slot="{ active, selected }"
+                    as="template"
+                  >
+                    <li :class="[active ? 'bg-blue-100 text-blue-900' : '', 'p-1 cursor-default']">
+                      <span v-show="selected" class="absolute left-0 pl-0.5 text-blue-600">
+                        <CheckIcon class="size-5" aria-hidden="true" />
+                      </span>
+                      <p :class="[selected ? 'font-medium' : 'font-normal', 'pl-5 truncate']">
+                        {{ collection.title }}
+                      </p>
+                    </li>
+                  </ListboxOption>
+                </template>
+                <ListboxOption v-else disabled class="p-1 opacity-70 cursor-default">
+                  Create collections
+                </ListboxOption>
+              </ListboxOptions>
+            </transition>
+          </div>
+        </Listbox>
+      </section>
+    </div>
+
+    <div class="flex flex-col items-center gap-5 col-span-2 row-start-2">
+      <div v-if="newProductMutation.isPending.value">Creating new product...</div>
+      <form
+        v-else
+        id="newProductForm"
+        @submit.prevent="handleSubmit"
+        class="w-full text-sm flex flex-col gap-5"
+      >
+        <!-- Title and Description -->
+        <TitleAndDescription v-model:title="title" v-model:description="description" />
+
+        <section class="bg-light rounded-xl shadow p-3">
+          <h2 class="font-semibold mb-4">Pricing</h2>
+          <label for="price">Price</label>
+          <PriceInput input-id="price" v-model="defaultVariant.price" :currency-symbol="symbol" />
+        </section>
+        <InventoryPanel
+          v-model:sku="defaultVariant.sku"
+          v-model:quantity="defaultVariant.quantity"
+        />
+
+        <!-- Options section -->
+        <section class="bg-light rounded-xl shadow-lg">
+          <div class="p-3 border-b border-gray-200">
+            <h2 class="font-semibold">Options</h2>
+            <p class="text-xs">Define options for the product, e.g. size, color, etc.</p>
+          </div>
+          <CreateProductOptionsPanel v-model:options="options" v-model:variants="variants" />
+        </section>
+
+        <!-- Variants table -->
+        <section v-if="validOptions.length">
+          <EditVariantDialog
+            v-model:is-open="isEditVariantOpen"
+            :currency-symbol="currencyInfo.symbol"
+            :variant-id:="activeEditVariant?.id"
+            :inventory-quantity="activeEditVariant?.inventoryQuantity"
+            :sku="activeEditVariant?.sku"
+            :price="activeEditVariant?.price"
+            @save-variant-edit="saveVariantEdit"
+          />
+          <TableComponent :table="variantsTable" :is-loading="false" />
+        </section>
+      </form>
     </div>
   </div>
 </template>
+
+<style lang="css" module>
+.grid-container {
+  grid-template-rows: min-content auto;
+}
+</style>
