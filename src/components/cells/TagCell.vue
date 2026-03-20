ada755412b666bc50911e40f71b64fb1d081f6d6
elijahcea <elijah.cea01@gmail.com>
2026-03-20 11:27:22 -0400

    create tagCell

---
+++ src/components/cells/TagCell.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ElTag } from 'element-plus'
+
+const { tagItems } = defineProps<{ tagItems: { name: string; key: string }[] }>()
+</script>
+
+<template>
+  <div class="flex gap-1">
+    <el-tag v-for="item in tagItems" :key="item.key">{{ item.name }}</el-tag>
+  </div>
+</template>
