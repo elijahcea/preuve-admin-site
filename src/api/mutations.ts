23abcde439ac20fdd8eb1c2a2cc1308e146b9318
elijahcea <elijah.cea01@gmail.com>
2026-03-26 14:10:10 -0400

    require token arg for auth header in mutations

--- src/api/mutations.ts
+++ src/api/mutations.ts
@@ -1,17 +1,52 @@
-import type { CreateProductResponse, ProductCreateDTO } from '@/lib/types'
+import type { CreateProductResponse, ProductCreateDTO, ProductUpdateDTO } from '@/lib/types'
 import axios from 'axios'
 
-export async function postNewProduct(newProduct: ProductCreateDTO): Promise<CreateProductResponse> {
-  const response = await axios.post(`${import.meta.env.VITE_PRODUCTS_API_URL}`, newProduct)
+export async function postNewProduct(
+  token: string,
+  newProduct: ProductCreateDTO,
+): Promise<CreateProductResponse> {
+  const response = await axios.post(`${import.meta.env.VITE_PRODUCTS_API_URL}`, newProduct, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
   return response.data
 }
 
-export async function deleteProduct(productId: string) {
-  const response = await axios.delete(`${import.meta.env.VITE_PRODUCTS_API_URL}/${productId}`)
+export async function deleteProduct(token: string, productId: string) {
+  const response = await axios.delete(`${import.meta.env.VITE_PRODUCTS_API_URL}/${productId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
   return response.data
 }
 
-export async function deleteCollection(collectionId: string) {
-  const response = await axios.delete(`${import.meta.env.VITE_COLLECTIONS_API_URL}/${collectionId}`)
+export async function deleteCollection(token: string, collectionId: string) {
+  const response = await axios.delete(
+    `${import.meta.env.VITE_COLLECTIONS_API_URL}/${collectionId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  )
+  return response.data
+}
+
+export async function updateProduct(
+  token: string,
+  productId: string,
+  updatedProduct: ProductUpdateDTO,
+) {
+  const response = await axios.put(
+    `${import.meta.env.VITE_PRODUCTS_API_URL}/${productId}`,
+    updatedProduct,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  )
   return response.data
 }
