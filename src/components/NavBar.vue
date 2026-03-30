2905c0471affcbe23cf4e14cc1d4d6e29d080dbd
elijahcea <elijah.cea01@gmail.com>
2026-03-30 15:28:46 -0400

    update styling for active links in nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,6 +3,7 @@ import { ShoppingBagIcon, TagIcon, HomeIcon } from '@heroicons/vue/24/outline'
 
 const iconSize = 'size-5'
 const itemStyles = 'flex items-center px-2 py-1 gap-2 hover:bg-(--nav-bg-hover) rounded'
+const activeStyles = 'bg-light outline outline-gray-200'
 </script>
 
 <template>
@@ -10,15 +11,15 @@ const itemStyles = 'flex items-center px-2 py-1 gap-2 hover:bg-(--nav-bg-hover) 
     class="pt-3 px-2 min-w-3xs bg-(--nav-bg) inset-shadow-sm font-semibold border-r border-r-gray-200"
   >
     <ul>
-      <RouterLink to="/" :class="itemStyles">
+      <RouterLink to="/" :class="itemStyles" :active-class="activeStyles">
         <HomeIcon :class="iconSize"></HomeIcon>
         <p>Home</p>
       </RouterLink>
-      <RouterLink :to="{ name: 'products' }" :class="itemStyles">
+      <RouterLink :to="{ name: 'products' }" :class="itemStyles" :active-class="activeStyles">
         <ShoppingBagIcon :class="iconSize"></ShoppingBagIcon>
         <p>Products</p>
       </RouterLink>
-      <RouterLink :to="{ name: 'collections' }" :class="itemStyles">
+      <RouterLink :to="{ name: 'collections' }" :class="itemStyles" :active-class="activeStyles">
         <TagIcon :class="iconSize"></TagIcon>
         <p>Collections</p>
       </RouterLink>
@@ -27,10 +28,6 @@ const itemStyles = 'flex items-center px-2 py-1 gap-2 hover:bg-(--nav-bg-hover) 
 </template>
 
 <style scoped>
-.router-link-active {
-  background-color: var(--nav-bg-selected);
-}
-
 nav {
   --nav-bg: #ebebeb;
   --nav-bg-hover: #f1f1f1;
