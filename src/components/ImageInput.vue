733ef75d563cd5eee8d2cef2c8a313d227bd3f54
elijahcea <elijah.cea01@gmail.com>
2026-04-09 20:56:27 -0400

    style imageInput and image preview

--- src/components/ImageInput.vue
+++ src/components/ImageInput.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { XMarkIcon } from '@heroicons/vue/24/outline'
 import { ElMessage } from 'element-plus'
 import { computed, useTemplateRef } from 'vue'
 
-const file = defineModel<File | undefined>({ required: true })
+const { inputId } = defineProps<{ inputId: string }>()
+
+const file = defineModel<File | undefined>('file', { required: true })
 
 const fileInput = useTemplateRef('fileInput')
 
@@ -15,9 +18,10 @@ const clearFile = () => {
 }
 
 const handleFileChange = (e: Event) => {
+  console.log('change', e)
   const target = e.target as HTMLInputElement
   const files = target.files
-  if (files) {
+  if (files?.length) {
     if (files[0]!.size > 500000) {
       clearFile()
       ElMessage.error('Image size cannot exceed 500KB!')
@@ -29,16 +33,27 @@ const handleFileChange = (e: Event) => {
 </script>
 
 <template>
-  <input
-    ref="fileInput"
-    type="file"
-    id="featured-image"
-    name="featured-image"
-    accept="image/jpeg, image/png, .jpg, .jpeg, .png"
-    @change="handleFileChange"
-  />
-  <div v-if="file">
-    <button type="button" @click.prevent="clearFile">Clear File</button>
-    <img :src="filePreviewUrl" :alt="filePreviewAlt" />
+  <div class="flex flex-col gap-4">
+    <p class="text-xs">Image size cannot exceed 500KB. JPG/PNG images only</p>
+
+    <input
+      ref="fileInput"
+      type="file"
+      :id="inputId"
+      :name="inputId"
+      accept="image/jpeg, image/png, .jpg, .jpeg, .png, .webp"
+      class="text-sm text-grey-500 file:mr-4 file:py-1 file:px-2 file:rounded-md file:border-0 file:text-sm file:bg-blue-100 file:text-blue-700 hover:file:cursor-pointer hover:file:bg-blue-100/50 cursor-pointer"
+      @change.prevent="handleFileChange"
+    />
+
+    <div v-if="file" class="flex flex-col gap-4">
+      <div
+        class="h-20 flex justify-between p-2 border border-foreground/10 border-dashed rounded-md"
+      >
+        <img :src="filePreviewUrl" :alt="filePreviewAlt" class="max-w-full h-auto" />
+
+        <button type="button" @click.prevent="clearFile"><XMarkIcon class="size-4" /></button>
+      </div>
+    </div>
   </div>
 </template>
